b730e8e84e787505854ad0d8b0bdbc75cca615e1
Ayrton Bourn <ayrtonbourn@outlook.com>
2024-06-15 19:48:47 +0100

    nested response

--- heatmap/definitions/analysis.py
+++ heatmap/definitions/analysis.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
 
-
-class OpexEstimate(BaseModel):
+class AnnualCostEstimate(BaseModel):
     annual_heat_pump_elec_cost: float
     annual_gas_boiler_counterfactual_cost: float
     annual_carbon_kg_reduction: float
+
+class OpexEstimate(BaseModel):
+    annual: AnnualCostEstimate
+    
