3cdd4c560fe3342fd0a6afa32412be241bc787e7
Ayrton Bourn <ayrtonbourn@outlook.com>
2024-06-16 10:40:26 +0100

    added midx to db

--- heatmap/db.py
+++ heatmap/db.py
@@ -2,26 +2,30 @@ import datetime
 
 from sqlmodel import Session, select
 
-from heatmap.definitions.timeseries import PowerCarbonIntensity
+from heatmap.definitions.timeseries import PointInTimeRecord
 
 
 def get_records_within_dates(
-    session: Session, start_date: datetime.datetime, end_date: datetime.datetime
-) -> list[PowerCarbonIntensity]:
+    session: Session, 
+    start_date: datetime.datetime,
+    end_date: datetime.datetime,
+    schema: PointInTimeRecord,
+) -> list[PointInTimeRecord]:
     """
-    Retrieves records from the 'power_carbon_intensity' table where the 'start_time'
+    Retrieves records from the time-series tables where the 'start_time'
     falls within the specified start and end datetime.
 
     Args:
     session (Session): The SQLAlchemy session object used to execute queries.
     start_date (datetime): The start datetime for the query range.
     end_date (datetime): The end datetime for the query range.
+    schema (PointInTimeRecord): The schema object for the table to query.
 
     Returns:
-    List[PowerCarbonIntensity]: A list of PowerCarbonIntensity records within the given datetime range.
+    List[PointInTimeRecord]: A list of PointInTimeRecord records within the given datetime range.
     """
-    statement = select(PowerCarbonIntensity).where(
-        PowerCarbonIntensity.start_time >= start_date, PowerCarbonIntensity.start_time <= end_date
+    statement = select(schema).where(
+        schema.start_time >= start_date, schema.start_time <= end_date
     )
     results = session.scalars(statement).all()
     return results
