cd57e9359684989da0c7a34d49f173e04064a80b
Ben Watts <ben@kilowatts.io>
2024-06-16 06:50:21 +0100

    add cors to API

--- heatmap/api/app.py
+++ heatmap/api/app.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from heatmap.api import analysis, timeseries
 
@@ -15,6 +16,15 @@ static_dir = os.environ["STATIC_DIR"]
 app = FastAPI(
     title="Heat Geek Hackathon", description='Yo yo yo, this is the API for the Heat Rash team.', version="0.1.0"
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow any origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
 
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
