3a0ad7ad0fdb3acaf3ba8cd85e712b58e9806cbf
Ayrton Bourn <ayrtonbourn@outlook.com>
2024-06-16 12:40:32 +0100

    added standing charges

--- heatmap/api/analysis.py
+++ heatmap/api/analysis.py
@@ -13,7 +13,6 @@ def get_heat_geek_address_details(address: str, postcode: str) -> dict:
     url = f'https://hackathonapis-35klfnb33a-ew.a.run.app/property_info/{address}/{postcode}'
     return requests.get(url).json()
 
-
 @router.get("/opex-estimate")
 def get_opex_estimate(
     min_scops: str = '3.5',
@@ -24,7 +23,10 @@ def get_opex_estimate(
     max_cop: float = 7.5,
     n_households: int = 1,
     elec_unit_rate: float = 246.4,  # octopus 12M elec unit rate in £/MWh
-    gas_unit_rate: float = 59.3  # octopus 12M gas unit rate in £/MWh
+    elec_standing_charge: float = 0.3872,  # octopus 12M elec standing charge in £/day
+    gas_unit_rate: float = 59.3,  # octopus 12M gas unit rate in £/MWh
+    gas_standing_charge: float = 0.2998  # octopus 12M gas standing charge in £/day
+    # TODO add standing charges
 ) -> list[OpexEstimate]:
     temperature_fp = 'data/temperature_2020_2032.csv'
     min_scops = [float(scop) for scop in min_scops.split(",")]
@@ -47,9 +49,9 @@ def get_opex_estimate(
         )
 
         annual_heat_pump_elec_load = df_opex_estimate_ts['elec_load_kwh'].sum()/n_years
-        annual_heat_pump_elec_cost = annual_heat_pump_elec_load * elec_unit_rate / 1e3
+        annual_heat_pump_elec_cost = (annual_heat_pump_elec_load * elec_unit_rate / 1e3) + (elec_standing_charge * 365)
         annual_counterfactual_boiler_gas_load = annual_heat_kwh_consumption/0.8
-        annual_counterfactual_boiler_gas_cost = annual_counterfactual_boiler_gas_load * gas_unit_rate / 1e3
+        annual_counterfactual_boiler_gas_cost = (annual_counterfactual_boiler_gas_load * gas_unit_rate / 1e3) + (gas_standing_charge * 365)
  
         response_objs.append(OpexEstimate(
             min_scops=min_scop,
